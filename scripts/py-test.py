61cf09b365bf02bd18966b96fc6abfe80d00c55c
broilo <mateus.broilo90@gmial.com>
2024-10-29 11:37:27 -0300

    feat/create py-scripts to test black format

---
+++ scripts/py-test.py
@@ -0,0 +1,11 @@
+def saluteFunction( x: str, number: int) -> None:
+    hi = print(f"{number *   x }")
+    return hi
+
+#fmt:off
+def main():
+    hello  = saluteFunction(   'olááár ', 10)
+#fmt:on
+
+if __name__ == "__main__":
+    main()
